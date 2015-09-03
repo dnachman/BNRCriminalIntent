5dd366e3c21148172cea66365380d109066c6069
David Nachman <david@nachman.org>
2015-09-03 15:52:38 -0400

    chapter 10 - the easy way to pass data to a fragment (from hosting activity)

--- app/src/main/java/com/logicalenigma/bnrcriminalintent/CrimeActivity.java
+++ app/src/main/java/com/logicalenigma/bnrcriminalintent/CrimeActivity.java
@@ -1,12 +1,25 @@
 package com.logicalenigma.bnrcriminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID = "com.logicalenigma.bnrcriminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crime_id) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crime_id);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
         return new CrimeFragment();
     }
+
 
 }
