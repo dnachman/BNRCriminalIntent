16c7616d23e6cdbdfa4f70a88f70b36f401391a3
David Nachman <david@nachman.org>
2015-09-04 19:38:26 -0400

    end of chapter 13 - toolbar

--- app/src/main/java/com/logicalenigma/bnrcriminalintent/CrimeLab.java
+++ app/src/main/java/com/logicalenigma/bnrcriminalintent/CrimeLab.java
@@ -17,15 +17,9 @@ public class CrimeLab {
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
 
-        // fake data
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
     }
 
+    // singleton
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
@@ -45,5 +39,9 @@ public class CrimeLab {
             }
         }
         return null;
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 }
--- app/src/main/java/com/logicalenigma/bnrcriminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/logicalenigma/bnrcriminalintent/SingleFragmentActivity.java
@@ -2,13 +2,13 @@ package com.logicalenigma.bnrcriminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Abstract activity for hosting a single fragment
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
